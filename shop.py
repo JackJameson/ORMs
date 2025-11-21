5bc6675083b14a050be96fe974f0d4b8faab3db9
James Jackman <jamesdjackman@yahoo.com>
2025-11-20 20:25:23 -0500

    add orders

--- shop.py
+++ shop.py
@@ -37,13 +37,18 @@ Base.metadata.create_all(engine)
 
 session = Session(engine)
 
-new_user1 = User(name="John Doe", email="JohnDoe@email.com")
-new_user2 = User(name="Jane Doe", email="JaneDoe@email.com")
+# new_user1 = User(name="John Doe", email="JohnDoe@email.com")
+# new_user2 = User(name="Jane Doe", email="JaneDoe@email.com")
 
-new_product1 = Product(name="Laptop", price=1000)
-new_product2 = Product(name="Smartphone", price=500)
-new_product3 = Product(name="Tablet", price=300)
+# new_product1 = Product(name="Laptop", price=1000)
+# new_product2 = Product(name="Smartphone", price=500)
+# new_product3 = Product(name="Tablet", price=300)
 
+new_order1 = Order(user_id=1, product_id=2, quantity=1)
+new_order2 = Order(user_id=2, product_id=1, quantity=5)
+new_order3 = Order(user_id=1, product_id=3, quantity=10)
+new_order4 = Order(user_id=2, product_id=3, quantity=3)
 
-session.add_all([new_user1, new_user2, new_product1, new_product2, new_product3])
+# session.add_all([new_user1, new_user2, new_product1, new_product2, new_product3])
+session.add_all([new_order1, new_order2, new_order3, new_order4])
 session.commit()
