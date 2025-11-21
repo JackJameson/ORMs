49d4e1b55a7ac30c8db34ec76ac8b4126a9eb007
James Jackman <jamesdjackman@yahoo.com>
2025-11-20 20:08:26 -0500

    create product and order tables and create one to many relationships

--- shop.py
+++ shop.py
@@ -13,8 +13,25 @@ class User(Base):
     name = Column(String(50), nullable=False)
     email = Column(String(100), unique=True, nullable=False)
 
-    # One-to-Many: User -> List of Order objects
     orders = relationship("Order", back_populates="user", cascade="all, delete-orphan")
+    
+class Product(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    price = Column(Integer, nullable=False)
+    
+class Order(Base):
+    __tablename__ = "orders"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    product_id = Column(Integer, ForeignKey("products.id"))
+    quantity = Column(Integer, nullable=False)
+
+    user = relationship("User", back_populates="orders")
+    product = relationship("Product")
     
 Base.metadata.create_all(engine)
 
